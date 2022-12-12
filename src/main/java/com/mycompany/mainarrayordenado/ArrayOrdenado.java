10125c1b22b361167eddb3a584e0a9fd404fd74d
a22lucasmpg <a22lucasmpg@W10D-I8E30.mshome.net>
2022-12-12 12:19:27 +0100

    Inicial

---
+++ src/main/java/com/mycompany/mainarrayordenado/ArrayOrdenado.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mainarrayordenado;
+
+/**
+ *
+ * @author a22lucasmpg
+ */
+public class ArrayOrdenado {
+
+    //ATRIBUTO
+    private static int[] array;
+
+    //MÉTODOS
+    public ArrayOrdenado() {
+        
+        this.array = new int[] {3, 8, 4, 1, 0, 7};
+        
+    }
+
+    public static int[] getArray() {
+        return array;
+    }
+
+    public static void setArray(int[] array) {
+        ArrayOrdenado.array = array;
+    }
+
+    public void ordenarArray() {
+        int tmp;
+        for (int i = 0; i < getArray().length - 1; i++) {
+            for (int j = i + 1; j < getArray().length; j++) {
+                if (getArray()[i] > getArray()[j]) {
+                    tmp = getArray()[i];
+                    getArray()[i] = getArray()[j];
+                    getArray()[j] = tmp;
+                }
+            }
+        }
+    }
+
+    public void amosarArray() {
+        
+        for (int i = 0; i < getArray().length; i++){
+            System.out.print(getArray()[i]);
+            System.out.print(" ");
+        }
+    }
+   
+}
---
+++ src/main/java/com/mycompany/mainarrayordenado/MainArrayOrdenado.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.mainarrayordenado;
+
+/**
+ *
+ * @author a22lucasmpg
+ */
+public class MainArrayOrdenado {
+
+    public static void main(String[] args) {
+
+        ArrayOrdenado miArray = new ArrayOrdenado();
+        
+        System.out.println("ARRAY SIN ORDENAR: ");
+        miArray.amosarArray();
+        System.out.println("");
+        System.out.println("ARRAY ORDENADO: ");
+        miArray.ordenarArray();
+        miArray.amosarArray();
+        
+        
+    }
+}
